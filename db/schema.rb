a036e4c844e5187a370f837fc56597395b282110
Hatice Celik <haticelik119@gmail.com>
2024-12-23 14:08:08 +0300

    home page and edit page done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_11_123553) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_23_102705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,5 +18,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_11_123553) do
     t.datetime "updated_at", null: false
     t.time "created_add"
     t.time "updated_add"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end

f76a21c3f7140c0379780b78988cfba747ec553f
Hatice Celik <haticelik119@gmail.com>
2024-12-26 15:24:22 +0300

    restrict actions and helper method done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  def hello
-    render html: "helllooo"
+  # def hello
+  #   render html: "helllooo"
+  # end
+  # allow_browser versions: :modern
+  #
+  helper_method :current_user, :logged_in?
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  allow_browser versions: :modern
+
+
+  def logged_in?
+    !!current_user
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "bootstrap";
 import "@popperjs/core";
+// filepath: /Users/hatice/rails_project/rails_6/test_6/app/javascript/packs/application.js
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
